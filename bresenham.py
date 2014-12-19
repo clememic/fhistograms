ede8403cbb36636468594f8a407b0e2b1fb4e156
Michaël Clément <michael.clement@parisdescartes.fr>
2014-12-19 12:09:08 +0100

    List of all parallel lines going through an image

--- bresenham.py
+++ bresenham.py
@@ -81,3 +81,77 @@ def restrict(ii, jj, height, width):
         jj = jj[:right]
 
     return ii, jj
+
+
+def parallel_lines(theta, height, width):
+    """
+    Return the pixel coordinates of all `theta` oriented parallel lines going
+    through an image of given size.
+
+    Parameters
+    ----------
+    theta : float
+        The orientation angle in radians.
+    height, width : (int, int,)
+        The size of the image.
+
+    Returns
+    -------
+    lines : list
+        A list containing pixel coordinates of all parallel lines going through
+        the image.
+
+    """
+    # Draw an initial line of sufficient length
+    theta = math.fmod(theta, 2 * np.pi)
+    rho = 2 * max(height, width)
+    ii, jj = line(theta, rho)
+    # Consider the bottom-left corner to be the origin
+    ii = height - ii - 1
+
+    # Translate the initial line according to the orientation angle
+    # 1st quadrant from the bottom-left corner
+    if 0 <= theta <= np.pi / 2:
+        tx, ty = 0, 0
+    # 2nd quadrant from the bottom-right corner
+    elif np.pi / 2 < theta < np.pi:
+        tx, ty = 0, width - 1
+    # 3rd quadrant from the top-right corner
+    elif np.pi <= theta <= 3 * np.pi / 2:
+        tx, ty = 1 - height, width - 1
+    # 4th quadrant from the top-left corner
+    else:
+        tx, ty = 1 - height, 0
+    ii += tx
+    jj += ty
+
+    # Build the set of parallel lines by shifting the initial line
+    lines = []
+    ii_u, jj_u = ii_d, jj_d = restrict(ii, jj, height, width)
+    lines.append((ii_u, jj_u)) # initial line
+
+    # Shift the initial line downwards
+    shift = 1
+    while ii_u.size > 0 and jj_u.size > 0:
+        if abs(ii[-1] - ii[0]) <= abs(jj[-1] - jj[0]):
+            ii_u, jj_u = restrict(ii - shift, jj, height, width)
+        else:
+            ii_u, jj_u = restrict(ii, jj - shift, height, width)
+        if ii_u.size > 0 and jj_u.size > 0:
+            lines.append((ii_u, jj_u))
+        shift += 1
+
+    lines.reverse() # to keep a logical order
+
+    # Shift the initial line upwards
+    shift = 1
+    while ii_d.size > 0 and jj_d.size > 0:
+        if abs(ii[-1] - ii[0]) <= abs(jj[-1] - jj[0]):
+            ii_d, jj_d = restrict(ii + shift, jj, height, width)
+        else:
+            ii_d, jj_d = restrict(ii, jj + shift, height, width)
+        if ii_d.size > 0 and jj_d.size > 0:
+            lines.append((ii_d, jj_d))
+        shift += 1
+
+    return lines
